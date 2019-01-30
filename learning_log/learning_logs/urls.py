021f573659a2685bddfa71eec6ba7306981a7264
Strli <18792146966@163.com>
2019-01-30 21:37:13 +0800

    增加新增条目功能&&编辑条目功能

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url
 from . import views
 
@@ -10,5 +10,9 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     #path('topics/<int:topic_id>/', views.topic, name='topic')
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    #用于添加新主题、新条目
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    #编辑既有条目
+    re_path(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 def index(request):
     return render(request, "learning_logs/index.html")
@@ -38,3 +38,44 @@ def new_topic(request):
     
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
+    
+def new_entry(request, topic_id):
+    """为特定主题添加新条目"""
+    
+    topic = Topic.objects.get(pk=topic_id)
+    
+    if request.method != 'POST':
+        #未提交数据，创建一个新的表单
+        form = EntryForm()
+    else:
+        #post提交数据,对数据进行处理。存储到数据库中
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                args=(topic.id,)))
+                
+    context = {'topic':topic, 'form':form}            
+    return render(request, 'learning_logs/new_entry.html', context)
+    
+def edit_entry(request, entry_id):
+    """编辑既有条目"""
+    entry = Entry.objects.get(pk=entry_id)
+    topic = entry.topic
+    
+    if request.method != 'POST':
+        #未提交数据，使用当前条目填充数据
+        form = EntryForm(instance=entry)
+    else:
+        #post提交数据，对原有条目实例进行替换
+        form = EntryForm(instance=entry, data=request.POST)  #不要忘记，表单提交数据存放于request.POST中
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                args=(topic.id, )))
+                
+    context = {'form':form, 'topic':topic, 'entry':entry}
+    return render(request, 'learning_logs/edit_entry.html', context)
+            
