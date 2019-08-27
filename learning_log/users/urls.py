cb81c9ddcdeb799c3357919946238ffc1b87b3d2
Strli <18792146966@163.com>
2019-08-27 22:03:05 +0800

    day2:新增注销在用户模块，以及用户注册模块

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.views import LoginView
+
+from . import views
 
 app_name = "users"
 
 urlpatterns = [
     # 登陆界面
-    url(r'login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    # 登出界面
+    path('logout/', views.logout_view, name='logout'),
+    # 注册页面
+    path('register/', views.register, name='register'),
 ]
