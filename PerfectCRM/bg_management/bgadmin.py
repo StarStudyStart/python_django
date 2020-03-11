ca7db38d2a74a4ec1d2d072d0f0877df7f3b1ec5
Strli <18792146966@163.com>
2020-03-11 23:11:00 +0800

    day2:自定义后台管理程序

--- PerfectCRM/bg_management/bgadmin.py
+++ PerfectCRM/bg_management/bgadmin.py
@@ -4,8 +4,28 @@
 Author  : yabinli
 Time    : 2020/3/11 0011 20:58
 """
+from crm import models
 
-class BGAdmin(object):
-    list_display = ()
+enabled_admins = {}
 
 
+class BaseAdmin(object):
+    list_display = []
+    list_filter = []
+
+
+class CustomerAdmin(BaseAdmin):
+    list_display = ['id', 'contact']
+    list_filter = ['name', ]
+
+
+def register(model_class, admin_class=None):
+    app_name = model_class._meta.app_label
+    model_name = model_class._meta.model_name
+    if app_name not in enabled_admins:
+        enabled_admins[app_name] = {}
+    admin_class.model = model_class
+    enabled_admins[app_name][model_name] = admin_class
+
+
+register(models.Customer, CustomerAdmin)
--- PerfectCRM/bg_management/views.py
+++ PerfectCRM/bg_management/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from bg_management import bgadmin
 # Create your views here.
 def index(request):
-    return render(request,'bg_management/index.html')
+    admins = bgadmin.enabled_admins
+    print(bgadmin.enabled_admins['crm']['customer'].model)
+    return render(request,'bg_management/index.html', {'admins':admins})
